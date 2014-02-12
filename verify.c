915e0bd211306ee628107a6e61acabe0fbf6ab54
John Tromp <tromp@Johns-iMac.local>
2014-02-11 23:51:19 -0500

    refactor verify

--- verify.c
+++ verify.c
@@ -8,31 +8,25 @@ int main(int argc, char **argv) {
   setheader(header);
   printf("Verifying size %d proof for cuckoo%d%d(\"%s\") with %d edges\n",
                PROOFSIZE, SIZEMULT, SIZESHIFT, header, EASINESS);
-  unsigned us[PROOFSIZE], vs[PROOFSIZE], i, nonce, sum;
-  for (int n = sum = 0; n < PROOFSIZE; n++) {
+  unsigned us[PROOFSIZE], vs[PROOFSIZE], i = 0, n, nonce, sum;
+  for (n = sum = 0; n < PROOFSIZE; n++) {
     assert(scanf("%*d %x (%*d,%*d)\n", &nonce) == 1);
     assert(nonce < EASINESS);
     sum = (sum + nonce) % EASINESS;
     sipedge(nonce, &us[n], &vs[n]);
   }
-  for (int n = i = 0; n < PROOFSIZE; n += 2) {
-    assert(n == 0 || i != 0);
-    int j = i; // find unique other j with same vs[j]
-    for (int k = 0; k < PROOFSIZE; k++)
-      if (k != i && vs[k] == vs[i]) {
-        assert(j == i);
-        j = k;
-      }
+  do {  // follow cycle until we return to i==0; n edges left to visit
+    int j = i;
+    for (int k = 0; k < PROOFSIZE; k++) // find unique other j with same vs[j]
+      if (k != i && vs[k] == vs[i]) { assert(j == i); j = k; }
     assert(j != i);
-    i = j; // find unique other i with same us[i]
-    for (int k = 0; k < PROOFSIZE; k++)
-      if (k != j &&  us[k] == us[j]) {
-        assert(i == j);
-        i = k;
-      }
+    i = j;
+    for (int k = 0; k < PROOFSIZE; k++) // find unique other i with same us[i]
+      if (k != j && us[k] == us[j]) { assert(i == j); i = k; }
     assert(i != j);
-  }
-  assert(i == 0);
-  printf("Verified with hash(%u)=%lx\n", sum, siphash(sum));
+    n -= 2;
+  } while (i);
+  assert(n == 0);
+  printf("Verified with hash(%u)=%llx\n", sum, siphash(sum));
   return 0;
 }
