530b3061f1992b7e92314da360b79bba77a02e5b
John Tromp <tromp@Johns-iMac.local>
2014-04-22 00:35:47 -0400

    research

--- private/comp.pl
+++ private/comp.pl
@@ -11,14 +11,26 @@ sub ent {
   return ent1($p) + ent1(1-$p);
 }
 
-my $f = 20;
+my $f = 0;
+my @comp;
 while (<>) {
   next unless /8 part V0 load (\d+)/;
   my $load = $1;
-  my $nc = 3200 * $f/100.0;
+  my $nc = 3200 * ++$f/100.0;
   my $nunc = 3200-$nc;
   my $p = ($load-$nunc)/$nc;
   my $e = ent($p);
-  print "$f $load $p $e\n";
-  $f++;
+  $comp[$f] = $e;
+  # printf("%2d %4d %3.4lf %3.4lf\n",$f,$load,$p,$e);
+}
+for my $mp (19..99) { # memory percentage
+  printf("\nmp = %2d ",$mp);
+  my $i;
+  my $np = $mp;   # nonce percentage
+  for ($i=0; $np < 100;  $i++) {
+    my $c = $comp[$mp];
+    $np += (1-$c)*$mp;
+    $np = int($np);
+    printf("%2d ",$np);
+  }
 }
