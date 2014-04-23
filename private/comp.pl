a11a01c87dd1dd4ece65a2a7b894e275bfc5c9cb
tromp <john.tromp@gmail.com>
2014-04-22 23:23:34 -0400

    rewrite time-memory trade-off section adding new data

--- private/comp.pl
+++ private/comp.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+use POSIX;
 use strict;
 
 sub ent1 {
@@ -13,6 +14,7 @@ sub ent {
 
 my $f = 0;
 my @comp;
+my @len;
 while (<>) {
   next unless /8 part V0 load (\d+)/;
   my $load = $1;
@@ -22,6 +24,7 @@ while (<>) {
   my $e = ent($p);
   $comp[$f] = $e;
   # printf("%2d %4d %3.4lf %3.4lf\n",$f,$load,$p,$e);
+  printf("(%2d,%0.4lf)%s",$f, $e, $f % 5 ? " " : "\n");
 }
 for my $mp (19..99) { # memory percentage
   printf("\nmp = %2d ",$mp);
@@ -31,6 +34,20 @@ for my $mp (19..99) { # memory percentage
     my $c = $comp[$mp];
     $np += (1-$c)*$mp;
     $np = int($np);
-    printf("%2d ",$np);
   }
+  $len[$mp] = $i;
+  # printf("%2d %3d",$mp, $i);
 }
+for my $tot (25..100) { # memory percentage
+  my $best=999999;
+  my $bmp;
+  for my $mp (19..$tot-1) { # memory percentage
+    my $cost = $len[$mp] * 16*int(ceil(200/($tot-$mp)));
+    next unless $cost < $best;
+    $best = $cost;
+    $bmp = $mp;
+  }
+  printf("(%2d,%3d)%s",$tot, $best, $tot % 5 ? " " : "\n");
+  # printf("(%2d,%3d)%c",$tot, $bmp, $tot%5?" ":"\n");
+}
+print "\n";
