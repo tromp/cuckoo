0f7bb592e1817563b3dc14c70dd3a4a9c3f2b2af
John Tromp <john.tromp@gmail.com>
2018-01-18 12:25:11 +0100

    default to cuckoo30

--- src/cuckoo.h
+++ src/cuckoo.h
@@ -14,7 +14,7 @@
 #ifndef EDGEBITS
 // the main parameter is the 2-log of the graph size,
 // which is the size in bits of the node identifiers
-#define EDGEBITS 27
+#define EDGEBITS 29
 #endif
 #ifndef PROOFSIZE
 // the next most important parameter is the (even) length
