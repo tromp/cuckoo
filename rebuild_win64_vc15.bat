75c686cdc40fd71001d522d20e69de03e1c7c8a9
svante <svante.karlsson@csi.se>
2018-11-23 14:58:54 +0100

    minimum sm_35

--- rebuild_win64_vc15.bat
+++ rebuild_win64_vc15.bat
@@ -9,7 +9,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tool
 mkdir  build 
 cd build
 
-cmake -G "Visual Studio 15 Win64" -DCMAKE_CUDA_FLAGS="-arch=sm_30" .. 
+cmake -G "Visual Studio 15 Win64" -DCMAKE_CUDA_FLAGS="-arch=sm_35" .. 
 
 msbuild ALL_BUILD.vcxproj /p:Configuration=Debug /p:Platform=x64 
 msbuild ALL_BUILD.vcxproj /p:Configuration=Release /p:Platform=x64
